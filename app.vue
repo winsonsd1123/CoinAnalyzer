d878b6d044557cd80dbe69051745ba1112775a7b
winsonsd1123 <17403933@qq.com>
2025-03-31 15:11:57 +0800

    Update files

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen bg-white">
-    <header class="bg-blue-600 text-white py-4 shadow-lg">
+    <header class="bg-blue-600 text-white py-3 shadow-lg">
       <div class="container mx-auto px-4 flex justify-between items-center">
         <div>
           <h1 class="text-2xl font-bold">欢迎来到分析启航家</h1>
